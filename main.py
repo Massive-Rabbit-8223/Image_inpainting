91ce547fd439e057d43ec6e12d610e9304551fcb
AFL_mederitsch <patrick.mederitsch.pm@gmail.com>
2022-06-30 22:44:58 +0200

    implemented baseline model (CAE)

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 from pickletools import optimize
 import splitfolders
 import json
+import h5py
 
 from utils.validate import validate_images
 from dataset_generator import create_dataset
-from models import Autoencoder, train_network
+from models import Autoencoder, train_network, create_data_dict
 
 import torch
 import torch.nn as nn
@@ -35,23 +36,25 @@ config = {
         "train_val_split_ratio": (0.8, 0.2)
     },
     "Model": {
-        "lr": 1e-3,
+        "lr": 5e-4,
         "path_to_model": os.path.join(path_to_current_experiment, "model.pth")
     },
     "Training": {
-        "epochs": 10,
-        "batch_size": 256
+        "epochs": 200,
+        "batch_size": 512
     }
 }
 
 FILTER_IMAGES = False
 CREATE_DATASET = False
-TRAIN_MODEL = True
+TRAIN_MODEL = False
 LOG_MODEL = False
+CREATE_DATA_DICTS = True
 
 train_input_dir = os.path.join(os.path.join(config["Data"]["data_dir"], config["Data"]["train_val_split_folder"]), "train/")
 val_input_dir = os.path.join(os.path.join(config["Data"]["data_dir"], config["Data"]["train_val_split_folder"]), "val/")
-
+path_to_train_data_dict = path_to_current_experiment + "/train_data_dict.hdf5"
+path_to_val_data_dict = path_to_current_experiment + "/val_data_dict.hdf5"
 
 if CREATE_DATASET == True:
     if FILTER_IMAGES == True:
@@ -97,30 +100,74 @@ else:
     print(f"val_dataset length: {len(val_dataset)}")
 
 train_dataloader = DataLoader(train_dataset, batch_size=config["Training"]["batch_size"], shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=config["Training"]["batch_size"], shuffle=False)
+val_dataloader = DataLoader(val_dataset, batch_size=len(val_dataset), shuffle=False)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#device = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = 'cpu'
 print(f'Using {device} device')
 
-model = Autoencoder(device)
+model = Autoencoder()
 optimizer = optim.Adam(model.parameters(), lr=config["Model"]["lr"])
 criterion = nn.MSELoss()
+criterion_individual = nn.MSELoss(reduction='none')
+model.to(device)
 
 if TRAIN_MODEL == True:
     train_network(
         model=model,
         optimizer=optimizer,
         criterion=criterion,
-        epochs=config["Training"]["epochs"],
+        n_epochs=config["Training"]["epochs"],
         train_dataloader=train_dataloader,
-        val_dataloader=val_dataloader
+        val_dataloader=val_dataloader,
+        device=device,
+        experiment_id="test_01"
     )
 
     torch.save(model.state_dict(), config["Model"]["path_to_model"])
+else:
+    model.load_state_dict(torch.load(config["Model"]["path_to_model"]))
+
+## File handling Utils ##
+
+def save_dict_hdf5(dict_to_save, file_name):
+    with h5py.File(file_name, 'w') as f:
+        for key, value in dict_to_save.items():
+            f.create_dataset(key, data=value)
+
+def load_dict_hdf5(file_name):
+    dictionary = {}
+    with h5py.File(file_name, 'r') as f:
+        for key in f.keys():
+            dictionary[key] = f[key][:]
+    
+    return dictionary
 
 def save_config_json(config_dict, file_name="config.json"):
     with open(os.path.join(path_to_current_experiment, file_name), 'w') as f:
         json.dump(config_dict, f, indent = 4)
 
 save_config_json(config)
+
+if CREATE_DATA_DICTS == True:
+    print("Creating Training Data Dict...")
+    train_data_dict = create_data_dict(
+        model=model, 
+        criterion=criterion_individual,
+        dataloader=train_dataloader,
+        device=device
+    )
+    print("Saving Training Data Dict...")
+    save_dict_hdf5(train_data_dict, path_to_train_data_dict)
+
+    print("Creating Val Data Dict...")
+    val_data_dict = create_data_dict(
+        model=model, 
+        criterion=criterion_individual,
+        dataloader=val_dataloader,
+        device=device
+    )
+    print("Saving Val Data Dict...")
+    save_dict_hdf5(val_data_dict, path_to_val_data_dict)
+
+print("Run Finished!")
--- models.py
+++ models.py
@@ -1,14 +1,17 @@
+from turtle import pos, update
+from matplotlib.pyplot import axis
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
+import os
 
 class Autoencoder(nn.Module):
-    def __init__(self, device):
+    def __init__(self):
         super(Autoencoder, self).__init__()
-        self.device = device
 
         self.encoder = nn.Sequential( # like the Composition layer you built
             nn.Conv2d(3, 16, 3, stride=2, padding=1),
@@ -16,84 +19,113 @@ class Autoencoder(nn.Module):
             nn.Conv2d(16, 32, 3, stride=2, padding=1),
             nn.ReLU(),
             nn.Conv2d(32, 64, 7)
-        ).to(self.device)
+        )
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(64, 32, 7),
             nn.ReLU(),
             nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(16, 3, 3, stride=2, padding=1, output_padding=1),
-        ).to(self.device)
+        )
 
     def forward(self, x):
-        x = self.encoder(x).to(self.device)
-        x = self.decoder(x).to(self.device)
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
 
     def apply_init_weights(self):
         ## apply weight initialization ##
         pass
 
-def train_network(model, optimizer, criterion, epochs, train_dataloader, val_dataloader):
-    test_loss1_list = []
-    test_acc1_list = []
-    epoch_list = []
-    for t in range(epochs):
-        #print(f"Epoch {t+1}\n-------------------------------")
+def train_network(model, optimizer, criterion, n_epochs, train_dataloader, val_dataloader, device, experiment_id):
+    # create tensorboard writer
+    writer = SummaryWriter(log_dir=os.path.join("results", experiment_id))
+    size_train_loader = len(train_dataloader)
+
+    # get initial evaluation on validation set
+    model.eval()
+    with torch.no_grad():
+        for f, t, i, k in val_dataloader:
+            pred = model(f.to(device))
+            test_loss = criterion(pred, i.to(device))
+    writer.add_scalar(tag="validation/loss", scalar_value=test_loss.cpu(), global_step=0)
+
+    with tqdm(total=len(train_dataloader), unit="batch") as pbar:
+        for epoch in range(n_epochs):
+            
+            counter = 0
+            model.train()
         
-        train_loop(t, train_dataloader, model, criterion, optimizer)
-        loss_acc_tuple = test_loop(val_dataloader, model, criterion, threshold=0.01)
+            for f, t, i, k in train_dataloader:
+                model.train()
+                update = epoch*size_train_loader + counter
+                pbar.set_description(f"Epoch {epoch}")
 
-        test_loss1_list.append(loss_acc_tuple[0])
-        test_acc1_list.append(loss_acc_tuple[1])
+                # Compute prediction and loss
+                pred = model(f.to(device))
+                loss = criterion(pred, i.to(device))
 
-        epoch_list.append(t+1)
+                # Backpropagation
+                loss.backward()
+                optimizer.step()
+
+                if (counter % 10) == 0:
+                    # Add losses as scalars to tensorboard
+                    writer.add_scalar(tag="training/loss", scalar_value=loss.cpu(), global_step=update)
+                    # Add parameters (weights) and gradients as arrays to tensorboard
+                    for i, (name, param) in enumerate(model.named_parameters()):
+                        writer.add_histogram(tag=f"training/param_{i} ({name})", values=param.cpu(), global_step=update)
+                        writer.add_histogram(tag=f"training/gradients_{i} ({name})", values=param.grad.cpu(), global_step=update)
+
+                # reset gradients
+                optimizer.zero_grad()
+                counter += 1
+                pbar.set_postfix(loss=loss.item())
+                pbar.update(1)
+
+            model.eval()
+            with torch.no_grad():
+                for f, t, i, k in val_dataloader:
+                    #print("f: ", f.shape)
+                    pred = model(f.to(device))
+                    test_loss = criterion(pred, i.to(device))
+            writer.add_scalar(tag="validation/loss", scalar_value=test_loss.cpu(), global_step=update)
+            pbar.refresh()
+            pbar.reset()
+
     print("Done!")
+    writer.close()
 
-def train_loop(epoch, dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    with tqdm(dataloader, unit="batch") as tepoch:
-        for f, t, i, k in tepoch:
-            tepoch.set_description(f"Epoch {epoch}")
-            # Compute prediction and loss
-            pred= model(f)
-            loss = loss_fn(pred, i.to(model.device))
+def create_data_dict(model, criterion, dataloader, device):
+    ## create data dicts ##
+    input_image_list = []
+    pred_image_list = []
+    target_image_list = []
+    loss_list = []
 
-            # Backpropagation
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            tepoch.set_postfix(loss=loss.item())
-            #if batch % 100 == 0:
-            #    loss, current = loss.item(), batch * len(f)
-            #    print(f"\rloss: {loss:>7f}  [{current:>5d}/{size:>5d}]",end='')
-
-
-def test_loop(dataloader, model, loss_fn, threshold):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss, correct = 0, 0
-
-    i = 0
+    model.eval()
     with torch.no_grad():
         for f, t, i, k in dataloader:
-            pred = model(f)
-            test_loss += loss_fn(pred, i.to(model.device)).item()
-            """
-            diff = torch.abs(pred-y)
+            pred = model(f.to(device))
+            loss = criterion(pred, i.to(device))
 
-            if torch.where(diff < threshold, 0, 1).sum() == 0:
-                correct += 1
-            else:
-                correct += 0
-            """
+            input_image_list.append(f)
+            pred_image_list.append(pred.cpu())
+            target_image_list.append(i)
+            loss_list.append(loss.cpu())
 
-            i += 1
-            
-    test_loss /= num_batches
-    #correct /= num_batches
-    #print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    #print(f"\rTest Error: \n Avg loss: {test_loss:>8f} \n", end='\n')
+        data_dict = {
+            "input_images": torch.cat(input_image_list, axis=0).cpu().detach().numpy(),
+            "pred_images": torch.cat(pred_image_list, axis=0).cpu().detach().numpy(),
+            "target_images": torch.cat(target_image_list, axis=0).cpu().detach().numpy(),
+            "loss_list": torch.cat(loss_list, axis=0).cpu().detach().numpy().mean(axis=(1,2,3))
+        }
 
-    return (test_loss, correct)
+    return data_dict
+
+def create_test_predictions(model, device):
+    """
+    Create predictions from test set and return in correct format, 
+    ready for upload to challenge server! 
+    """
+    pass
